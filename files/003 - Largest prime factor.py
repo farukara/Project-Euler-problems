f8eb00e1df7ad57a9276025cd41e0737fb993477
farukara <69348522+farukara@users.noreply.github.com>
2020-08-19 17:45:46 +0300

    problem 3 and 4 added

--- files/003 - Largest prime factor.py
+++ files/003 - Largest prime factor.py
@@ -1,10 +1,30 @@
+#! Python3
+# finds largest prime factor of a given number
 
-def isprime(number):
-    assign = True
-    for i in range (2,number):
-        if number % i == 0:
-            assign = False
-    return assign    
+def find_largest_prime_factor(n):
+    # find largest prime factor of n
+    def isprime(number):
+        # returns true if number is prime number
+        assign = True
+        for i in range (2,number):
+            if number % i == 0:
+                assign = False
+        return assign    
 
-for i in range(50):
-    print(i, ':', isprime(i))
+
+    if isprime(n):
+        print(n, "is already a prime number, don't have any factors")
+
+    for i in range(2, round(n/2+1)):
+        if (n/i)%1 == 0:
+            if isprime(int(n/i)):
+                break
+
+    return(int(n/i))
+
+
+    
+
+if __name__ == "__main__":
+    n = 13195
+    print('largest prime factor of ', n, 'is : ', find_largest_prime_factor(n))
---
+++ files/004 - Largest palindrome product.py
@@ -0,0 +1,29 @@
+#! Python3
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+def polindrome_product_finder(digits):
+    upper_limit = number_of_digits * 333
+    lower_limit = number_of_digits * 33
+    pairs_found = False
+
+    m = upper_limit
+    while m > lower_limit and pairs_found == False:
+        n = upper_limit
+
+        while n > lower_limit:
+            prod = m*n
+            if len(str(prod))%2 == 0:
+                if int(str(prod)[:int(len(str(prod))/2)])== int(str(prod)[::-1][:int(len(str(prod))/2)]):
+                    pairs_found = True
+                    break
+                    
+            n -= 1
+    
+        m -= 1
+    
+    print(m, n)
+
+if __name__ == '__main__':
+    number_of_digits = 3  # this line is to avoid hard coding, can accept user input
+    polindrome_product_finder(number_of_digits)
